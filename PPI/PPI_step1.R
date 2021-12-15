ddf96ef5d2f78c59d9e07f91f2616de433dee93f
Yi Wang <37149810+yiwang12@users.noreply.github.com>
2021-12-15 14:53:10 -0500

    Update PPI_step1.R

--- PPI/PPI_step1.R
+++ PPI/PPI_step1.R
@@ -2,12 +2,11 @@ library(readr)
 library(matrixStats)
 library(SummarizedExperiment)
 
-#original_files = "/Users/yiwang/Dropbox/project_Kasper/4_26_2019/threshold_PPI/data/dat_HURI_PPI_full.tsv"
-original_files = "/users/ywang/10_25_2019/PPI/data/dat_HURI_PPI_full.tsv"
-dir_output="/users/ywang/10_25_2019/PPI/data/"
-source("/users/ywang/10_25_2019/functions/functions_2d_quantile_no_surf_July_18.R")
-source("/users/ywang/10_25_2019/functions/functions_evaluation_Aug_5.R")
-dir_data="/users/ywang/10_25_2019/data/"
+original_files = "/data/dat_HURI_PPI_full.tsv"
+dir_output="/PPI/data/"
+source("/functions/functions_2d_quantile_no_surf_July_18.R")
+source("/functions/functions_evaluation_Aug_5.R")
+dir_data="/data/"
 
 HURI=read_tsv(original_files)
 #sum(HURI$Ensembl_gene_id_b=="ENSG00000000005")
@@ -56,33 +55,4 @@ for(i in 1:9){
 
 
 
-
-# 
-# 
-# 
-# 
-# ## filter out noncoding genes from PPI data
-# load("/users/ywang/8_25_2019/overleaf_code_data/PPI/data/humanProteinCodingGenes_ensembl.RData")#humanProteinCodingGenes_ensembl
-# PPI_coding=HURI[which(HURI$gene1 %in% humanProteinCodingGenes_ensembl & HURI$gene2%in% humanProteinCodingGenes_ensembl),]
-# save(PPI_coding,file=paste0(dir_output,"PPI_coding.RData" ))
-# 
-# 
-# ## keep only genes overlapped with GTEX data in PPI 
-# PPI_coding_overlap=PPI_coding[which(PPI_coding$gene1 %in% genes_ensemble_keep2_format & PPI_coding$gene2%in% genes_ensemble_keep2_format),]
-# ## match expression level to the pairs of PPI data
-# rownames(ave_logcpm_kp)=genes_ensemble_keep2_format
-# exp_level_gene1_PPI= ave_logcpm_kp[PPI_coding_overlap$gene1]
-# exp_level_gene2_PPI= ave_logcpm_kp[PPI_coding_overlap$gene2]
-# ave_exp_PPI=(exp_level_gene1_PPI+exp_level_gene2_PPI)/2
-# save(ave_exp_PPI,file=paste0(dir_output,i,"_th_tissue_ave_exp_PPI.RData" ))
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
-# 
+ 
